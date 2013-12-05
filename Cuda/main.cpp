002e23273a315fd6213455904b007bc0a3fe54ab
Matthew Miller <matt.b.miller@gmail.com>
2013-12-04 22:26:16 -0800

    Timestamp

--- Cuda/main.cpp
+++ Cuda/main.cpp
@@ -8,6 +8,12 @@
 #include <time.h>
 
 #include "readjpeg.h"
+double timestamp()
+{
+  struct timeval tv;
+  gettimeofday (&tv, 0);
+  return tv.tv_sec + 1e-6*tv.tv_usec;
+}
 typedef struct
 {
         float r;
